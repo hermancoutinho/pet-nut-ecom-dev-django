ece1584d3a6b0d1404865ffa98d1f368f782da6e
hermancoutinho <herman.coutinho@gmail.com>
2022-08-02 14:51:52 +0400

    added pagination

--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import  EmptyPage, PageNotAnInteger, Paginator
 from .models import Product
 from category.models import Category
 from cart.models import Cart, CartItem
@@ -13,13 +14,19 @@ def store(request, category_slug=None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
+        paginator = Paginator(products, 1) # Showing 6 products per page
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
+        paginator = Paginator(products, 3) # Showing 6 products per page
+        page = request.GET.get('page')
+        paged_product = paginator.get_page(page)
         product_count = products.count
     
     context = {
-        'products': products,
+        'products': paged_product,
         'product_count': product_count,
     }
     return render(request, 'store/store.html',context)
